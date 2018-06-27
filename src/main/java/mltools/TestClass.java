fd580823b580700edb629cda649d7cce95cfb65c
zhangcheng <zhangchengclfal@163.com>
2018-06-27 21:04:21 +0800

    添加测试

---
+++ src/main/java/mltools/TestClass.java
@@ -0,0 +1,13 @@
+package mltools;
+
+public class TestClass {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
